ab88eca4b4b2d0452cec7f37f16487a2c6f0f1f8
Nick S <oart4k@gmail.com>
2023-12-03 09:39:57 -0500

    feature: star 1 completed

--- 2023/star1/solution.go
+++ 2023/star1/solution.go
@@ -1,6 +1,35 @@
 package star1
 
+import (
+	"strconv"
+)
+
 func Fun(input []string) interface{} {
 
-	return nil
+	result := 0
+
+	for _, line := range input {
+		num := []rune{}
+		for _, char := range line {
+			if char >= '0' && char <= '9' {
+				switch len(num) {
+				case 0, 1:
+					num = append(num, char)
+				case 2:
+					num[1] = char
+				}
+			}
+		}
+
+		switch len(num) {
+		case 0:
+			continue
+		case 1:
+			num = append(num, num[0])
+		}
+		nint, _ := strconv.Atoi(string(num))
+		result += nint
+	}
+
+	return result
 }
